f9e6b3675cacc3bd809765b6b712e7a8ba0615bb
dogankayaa1 <109869623+dogankayaa1@users.noreply.github.com>
2022-07-23 20:14:37 +0300

    Add files via upload

---
+++ day915/js/function.js
@@ -0,0 +1,12 @@
+var namee = document.getElementById("name").value;
+var email = document.getElementById("email").value;
+var subject = document.getElementById("subject").value;
+var mesage = document.getElementById("mesage").value;
+var submit = document.getElementById("submit");
+var all = namee,email,subject;
+
+function submitForm(e){
+    var form = all;
+    alert("form Submit")
+
+}
